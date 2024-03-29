cad7abfab19d6c340cdccacfade2f08b56c58e76
SaifRashid <42101711+SaifRashid@users.noreply.github.com>
2024-03-29 00:58:59 -0400

    Implemented interface and data class

--- app/src/main/java/com/example/ticketfinderapp/TicketMasterService.kt
+++ app/src/main/java/com/example/ticketfinderapp/TicketMasterService.kt
@@ -6,6 +6,7 @@ import retrofit2.http.Query
 
 interface TicketMasterService {
     // https://app.ticketmaster.com/discovery/v2/events.json?apikey=Ayl9reEX22B94IUU01mLxM89kAE1H6ia
+    // https://app.ticketmaster.com/discovery/v2/events.json?keyword=music&city=hartford&apikey=Ayl9reEX22B94IUU01mLxM89kAE1H6ia
 
     @GET(".")
     fun getData(@Query("keyword") keyword: String,@Query("city") city: String): Call<Any>
